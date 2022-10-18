a43c2917d45365b87aebd98daef4ef5ff17bd530
natedorgan <natedorgan@gmail.com>
2022-10-18 13:30:36 -0700

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -10,14 +10,14 @@ class User:
 # though I was unable to sort out how to do that in Python so this is a theoretical
 class Subscriber(User):
     address = ''
-    subscriber_since =
+    subscriber_since = ''
 
 # Guest is also a child of User and so inherits the same attributes
 # They also can be on or off the email list and it saves their age in case
 # there is age-restricted content I guess
 class Guest(User):
     email_list = True
-    age = 
+    age = ''
     
     
     
