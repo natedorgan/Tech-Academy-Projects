8169ff297e918bd09c8636d3c075055674ba1f3c
natedorgan <natedorgan@gmail.com>
2022-10-25 21:47:59 -0700

    Update web_page_generator.py

--- web_page_generator.py
+++ web_page_generator.py
@@ -6,15 +6,15 @@ class ParentWindow(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master.title("Web Page Generator")
-        self.lbl_entry = tk.Label(self.master,text='Website Text:')
-        self.lbl_entry.grid(row=0,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
-        self.txt_entry = tk.Entry(self.master,text='')
-        self.txt_entry.grid(row=1,column=0,rowspan=1,columnspan=2,padx=(30,40),pady=(0,0),sticky=N+E+W)
+        self.lbl_entry = tk.Label(self.master,text='Website Text:') #label for the entry box
+        self.lbl_entry.grid(row=0,column=0,padx=(27,0),pady=(10,0),sticky=N+W) #placement for said label
+        self.txt_entry = tk.Entry(self.master,text='') #the entry box
+        self.txt_entry.grid(row=1,column=0,rowspan=1,columnspan=2,padx=(30,40),pady=(0,0),sticky=N+E+W) #placement for said box
         self.btn = Button(self.master, text="Default HTML Page", width=30, height=2, command=self.defaultHTML)
         self.btn.grid(padx=(10,10) , pady=(10,10))
         
     def defaultHTML(self):
-        htmlText = self.txt_entry.get()
+        htmlText = self.txt_entry.get() #gets text from input
         htmlFile = open("index.html", "w")
         htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
         htmlFile.write(htmlContent)
